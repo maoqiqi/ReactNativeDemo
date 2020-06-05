d48cc3125ab5bbc7dffbb3010e093e5ae830edb5
maoqiqi <fengqi.mao.march@gmail.com>
2020-06-05 17:16:38 +0800

    first commit

---
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -0,0 +1,11 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "ReactNativeDemo";
+  }
+}
